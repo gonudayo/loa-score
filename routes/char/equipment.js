aaa6e002802a8e8d38ef6238b6cb47f12be8b7f9
gonudayo <kgw010119@gmail.com>
2024-01-27 01:02:40 +0900

    fix: gear set

--- routes/char/equipment.js
+++ routes/char/equipment.js
@@ -1,5 +1,51 @@
 const equipment = (arr) => {
-  let value = [];
+  let value = {
+    사멸: {
+      1: 0,
+      2: 0,
+      3: 0,
+    },
+    악몽: {
+      1: 0,
+      2: 0,
+      3: 0,
+    },
+    환각: {
+      1: 0,
+      2: 0,
+      3: 0,
+    },
+    구원: {
+      1: 0,
+      2: 0,
+      3: 0,
+    },
+    지배: {
+      1: 0,
+      2: 0,
+      3: 0,
+    },
+    갈망: {
+      1: 0,
+      2: 0,
+      3: 0,
+    },
+    배신: {
+      1: 0,
+      2: 0,
+      3: 0,
+    },
+    파괴: {
+      1: 0,
+      2: 0,
+      3: 0,
+    },
+    매혹: {
+      1: 0,
+      2: 0,
+      3: 0,
+    },
+  };
 
   if (arr === null) {
     return value;
@@ -9,7 +55,7 @@ const equipment = (arr) => {
     let tmp = arr[i].Tooltip.split(" <FONT COLOR='#FFD200'>Lv.");
     let set = tmp[0].slice(-2);
     let level = tmp[1].slice(0, 1);
-    value.push({ set, level });
+    ++value[set][level];
   }
 
   return value;
